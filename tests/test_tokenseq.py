d21204f32c407d21849cb5a4f118549a2f608704
Markus Konrad <post@mkonrad.net>
2023-05-03 15:38:32 +0200

    fixed cell output length in doc notebooks; fixed test_token_collocations_hypothesis

--- tests/test_tokenseq.py
+++ tests/test_tokenseq.py
@@ -584,7 +584,7 @@ def test_token_collocations_hypothesis(sentences, threshold, min_count, pass_emb
                 assert isinstance(row, tuple)
                 assert len(row) == 2
                 colloc, stat = row
-                assert isinstance(stat, float)
+                assert isinstance(stat, (float, int))
                 if threshold:
                     assert stat >= threshold
                 if statistic is identity:
